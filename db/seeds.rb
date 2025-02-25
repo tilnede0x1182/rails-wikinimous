97f5cbf8376b76c5bc37ad7cc88ae0f0e2939b37
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-02-25 17:02:38 +0100

    Exercice rails-wikinimous completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+# Supprime tous les articles existants pour éviter les doublons
+# Article.destroy_all
+
+# Crée 10 articles fictifs
+10.times do
+  Article.create(
+    title: Faker::Book.title,       # Génère un titre de livre aléatoire
+    content: Faker::Lorem.paragraphs(number: 3).join("\n\n")  # Génère 3 paragraphes de texte aléatoire
+  )
+end
+
+puts "10 articles fictifs ont été créés !"
